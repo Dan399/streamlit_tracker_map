7e6ad2bd3c388335ae8dff2f7de4948d9506120a
Dan Lopez Villegas <38848160+Dan399@users.noreply.github.com>
2023-10-16 17:36:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,6 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 @st.cache_data
 def get_data_blue():
     url = 'https://github.com/Dan399/streamlit_tracker_map/blob/0e1ae739e4a80bb64384a11752108f1a6657558b/Data/DataBluemessaging2023-2023.xlsx'
-    df = pd.read_csv(url, index_col=0)
     df_blue = pd.read_excel(url, 
     converters={'nrp':str, 'user':str, 'foto_domicilio.lat':float, 'foto_domicilio.lng':float})
     df_blue['fecha_accion_fiscal'] = pd.to_datetime(df_blue['fecha_accion_fiscal']).dt.date
